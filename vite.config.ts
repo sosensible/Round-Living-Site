9aa8bf267bae9c1eecd96da8497e072ca882d323
sosensible <sosensible@users.noreply.github.com>
2023-05-12 17:16:01 -0400

    Tests running.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/// <reference types="vitest" />
+
+// Configure Vitest (https://vitest.dev/config/)
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {},
   plugins: [vue()],
-})
+});
