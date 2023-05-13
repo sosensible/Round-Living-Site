5f2e892bf2939191316f8251346f91534ab2bd65
sosensible <sosensible@users.noreply.github.com>
2023-05-13 10:26:55 -0400

    Not working, going to redo from Main

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import View from './components/View.vue';
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <View element="span">Does this work?</View>
 </template>
 
 <style scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,21 @@
 
 // Configure Vitest (https://vitest.dev/config/)
 
+import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  test: {},
   plugins: [vue()],
+  test: {
+    globals: true,
+    setupFiles: 'src/setupTests.ts',
+    includeSource: ['src/**/*.{js,ts,vue}'],
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 });
